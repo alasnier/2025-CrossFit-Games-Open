8546707e9e8702de5a21ec8b5a3ffce9e8ab845c
Alex LASNIER <133754892+alasnier@users.noreply.github.com>
2025-01-31 10:09:42 +0100

    first graphic part done with streamlit

---
+++ pages/Wods_open2025.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+# Config default settings of the page.
+
+
+st.set_page_config(
+    page_title="Wods open2025",
+    layout="wide",
+    page_icon="2025",
+)
+
+st.header("2025 Open workouts have not been posted yet.")
